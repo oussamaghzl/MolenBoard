a77e8f02a751c71bf86d3b3b2a3d4017b9a75813
Anis <anisberchem1189@gmail.com>
2020-12-28 03:27:23 -0800

    Les roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id'
     ];
 
     /**
@@ -40,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    protected $table = "user";
+    public function roles(){
+        return $this->hasMany(Role::class, "role_id", "id");
+    }
 }
